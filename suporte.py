abea40fdbdf6c2c40f229c6bb4f3f3908740e4e7
JESUS <marcos.jesus.py25@gmail.com>
2025-09-30 16:13:52 -0300

    Create suporte.py

---
+++ suporte.py
@@ -0,0 +1,68 @@
+import flet as ft
+
+def suporte_view(page: ft.Page):
+    page.title = "Suporte"
+    page.theme_mode = ft.ThemeMode.DARK
+    page.window.width = 400
+    page.window.height = 700
+    page.window.min_width = 360
+    page.window.max_width = 500
+    page.window.min_height = 600
+    page.window.max_height = 800
+    page.padding = 0
+    page.window.center()
+
+    # ---------- Inputs ----------
+    envidas = ft.TextField(label="Nome de usuário", width=360)
+    mensagem = ft.TextField(
+        label="Mensagem",
+        multiline=True,
+        min_lines=4,
+        max_lines=8,
+        width=360,
+    )
+
+    # ---------- Função Enviar ----------
+    def enviar_click(e):
+      if envidas.value.strip() and mensagem.value.strip():
+        page.open( ft.SnackBar(
+            content=ft.Text("Enviado com sucesso!", color="white"),
+            bgcolor="green"
+        ))
+        # Limpar os campos após envio
+        envidas.value = ""
+        mensagem.value = ""
+      else:
+        page.open( ft.SnackBar(
+            content=ft.Text("Preencha todos os campos!", color="white"),
+            bgcolor="red"
+        ))
+        page.open = True
+        page.update()
+
+    # ---------- Layout ----------
+    conteudo = ft.Column(
+        [
+            ft.Text("SUPORTE", size=30, weight="bold"),
+            envidas,
+            mensagem,
+            ft.ElevatedButton("Enviar", on_click=enviar_click, width=300),
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+    )
+
+
+ 
+    
+    
+    return ft.View(
+    route="/suporte",
+    controls=[       ft.Container(
+            content=conteudo,
+            alignment=ft.alignment.center,
+            expand=True,
+        )],
+    vertical_alignment="center",
+    horizontal_alignment="center",
+        )
