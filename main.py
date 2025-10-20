d81106fcc69aca9ecedfe246f5022385fc807ee8
Kauã Pierre <auakpierre@gmail.com>
2025-10-20 17:39:34 -0300

    Refactor routing and add new views in main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import flet as ft
+from Carregamento import CarregamentoView
 from home import HomeView
 from login import LoginView
 from cadastro import CadastroView
-from notificação import View_notificacao
 from perfil import PerfilView
+from testeaulassss import aulasView
+from notificação import View_notificacao
+from suporte import suporte_view
 
 # CRIAR UM BOTAO PARA INSERIR UM SITE DE PORTFOLIO PARA CADA DEV COM AS INFORMAÇÕES E O LINK PARA GIT
 
@@ -14,18 +17,22 @@ def main(page: ft.Page):
     
     def route_change(e):
         page.views.clear()
-
-        if page.route == "/":
+        if page.route == "/login":
             page.views.append(LoginView(page))
         elif page.route == "/home":
             page.views.append(HomeView(page))
         elif page.route == "/cadastro":
-            page.views.append(CadastroView(page))
-        elif page.route == "/notificacao":
-            page.views.append(View_notificacao(page))    
+            page.views.append(CadastroView(page))    
         elif page.route == "/perfil":
-            page.views.append(View_perfil(page)) 
-
+            page.views.append(PerfilView(page)) 
+        elif page.route == "/":
+            page.views.append(CarregamentoView(page)) 
+        elif page.route == "/aulasView":
+            page.views.append(aulasView(page)) 
+        elif page.route == "/notificação":
+            page.views.append(View_notificacao(page)) 
+        elif page.route == "/suporte":
+            page.views.append(suporte_view(page))
         page.update()
 
    
