bdea5c4d352f832a739206039fb3b463f81994be
Victor Santana <victorsantana.rezende@gmail.com>
2025-10-23 17:48:36 -0300

    update

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import flet as ft
-from Carregamento import CarregamentoView
 from home import HomeView
+from desempenho import DesempenhoView
 from login import LoginView
 from cadastro import CadastroView
 from perfil import PerfilView
-from testeaulassss import aulasView
 from notificação import View_notificacao
 from suporte import suporte_view
+
 
 # CRIAR UM BOTAO PARA INSERIR UM SITE DE PORTFOLIO PARA CADA DEV COM AS INFORMAÇÕES E O LINK PARA GIT
 
@@ -17,7 +17,7 @@ def main(page: ft.Page):
     
     def route_change(e):
         page.views.clear()
-        if page.route == "/login":
+        if page.route == "/":
             page.views.append(LoginView(page))
         elif page.route == "/home":
             page.views.append(HomeView(page))
@@ -25,19 +25,18 @@ def main(page: ft.Page):
             page.views.append(CadastroView(page))    
         elif page.route == "/perfil":
             page.views.append(PerfilView(page)) 
-        elif page.route == "/":
-            page.views.append(CarregamentoView(page)) 
-        elif page.route == "/aulasView":
-            page.views.append(aulasView(page)) 
         elif page.route == "/notificação":
             page.views.append(View_notificacao(page)) 
         elif page.route == "/suporte":
             page.views.append(suporte_view(page))
+        elif page.route == "/desempenho":
+            page.views.append(DesempenhoView(page))
         page.update()
+        
 
    
     page.on_route_change = route_change
     page.go("/")  
 
-ft.app(target=main)
+ft.app(target=main,assets_dir="assets")
 
